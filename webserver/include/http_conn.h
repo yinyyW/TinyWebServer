64fb5d6cb3388a1e50bcd7a8e6fb1f40ac890bc7
Yinyu Wu <yinyu.wu@student.unsw.edu.au>
2023-01-11 22:31:40 +1100

    http_conn

--- webserver/include/http_conn.h
+++ webserver/include/http_conn.h
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/mman.h>
+#include <sys/epoll.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -24,7 +25,7 @@ class http_conn {
         static const int READ_BUFFER_SIZE = 2048;
         static const int WRITE_BUFFER_SIZE = 1024;
         enum METHOD { GET = 0, POST, HEAD, PUT, DELETE };
-        enum CHECK_STATE { CHECK_STATE_REQUEST_LINE = 0, CHECK_STATE_HEADER, 
+        enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, 
                             CHECK_STATE_CONTENT };
         enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE,
                         FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
--- webserver/src/http_conn.cpp
+++ webserver/src/http_conn.cpp
@@ -18,10 +18,228 @@ int setnonblocking(int fd) {
     return old_option;
 }
 
-void add_fd(int epollfd, int fd, bool one_shot) {
-    
+// ############# EPOLL OPERATION #############
+void addfd(int epollfd, int fd, bool one_shot) {
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+    if (one_shot) {
+        event.events |= EPOLLONESHOT;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
 }
+
+void removefd(int epollfd, int fd) {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+
+void modfd(int epollfd, int fd, int ev) {
+    epoll_event event;
+    event.data.fd = fd;
+    event.events |= ev | EPOLLIN | EPOLLRDHUP;
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+}
+
+// ############# HTTP_CONN CLASS METHODS IMPLEMENTATION #############
 
 // http_conn class static member initialization
 int http_conn::m_epollfd = -1;
 int http_conn::m_user_count = 0;
+
+void http_conn::close_conn(bool real_close) {
+    if (real_close && m_sockfd != -1) {
+        removefd(m_epollfd, m_sockfd);
+        m_sockfd = -1;
+        --m_user_count;
+    }
+}
+
+void http_conn::init(int sockfd, const sockaddr_in &addr) {
+    m_sockfd = sockfd;
+    m_address = addr;
+    // debug
+    // int reuse = 1;
+    // setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    addfd(m_epollfd, sockfd, true);
+    ++m_user_count;
+    init();
+}
+
+void http_conn::init() {
+    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_linger = false;
+    
+    m_method = GET;
+    m_url = 0;
+    m_version = 0;
+    m_content_length = 0;
+    m_host = 0;
+    m_start_line = 0;
+    m_checked_idx = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+    
+    memset(m_read_buf, '\0', READ_BUFFER_SIZE);
+    memset(m_write_buf, '\0', WRITE_BUFFER_SIZE);
+    memset(m_real_file, '\0', FILENAME_LEN);
+}
+
+http_conn::LINE_STATUS http_conn::parse_line() {
+    char temp;
+    for (; m_checked_idx < m_read_idx; ++m_checked_idx) {
+        temp = m_read_buf[m_checked_idx];
+        if (temp == '\r') {
+            if (m_checked_idx + 1 == m_read_idx) {
+                return LINE_OPEN;
+            } else if (m_read_buf[m_checked_idx + 1] == '\n') {
+                m_read_buf[m_checked_idx++] = '\0';
+                m_read_buf[m_checked_idx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        } else if (temp == '\n') {
+            if (m_checked_idx > 1 && m_read_buf[m_checked_idx - 1] == '\r') {
+                m_read_buf[m_checked_idx-1] = '\0';
+                m_read_buf[m_checked_idx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+    }
+    return LINE_OPEN;
+}
+
+http_conn::HTTP_CODE http_conn::parse_request_line(char* text) {
+    m_url = strpbrk(text, " \t");
+    if (!m_url) {
+        return BAD_REQUEST;
+    }
+    *m_url++ = '\0';
+    
+    // request's method
+    char* method = text;
+    if (strcasecmp(method, "GET") == 0) {
+        m_method = GET;
+        std::cout << "request method is GET\n";
+    } else if (strcasecmp(method, "POST")) {
+        m_method = POST;
+        std::cout << "request method is POST\n";
+    } else {
+        return BAD_REQUEST;
+    }
+    
+    // request's version, only support HTTP1.1
+    m_url += strspn(m_url, " \t");
+    m_version = strpbrk(m_url, " \t");
+    if (!m_version) {
+        return BAD_REQUEST;
+    }
+    *m_version++ = '\0';
+    m_version += strspn(m_version, " \t");
+    if (strcasecmp(m_version, "HTTP/1.1") != 0) {
+        return BAD_REQUEST;
+    }
+    
+    // request's url
+    if (strncasecmp(m_url, "http://", 7) == 0) {
+        m_url += 7;
+        m_url = strchr(m_url, '/');
+    }
+    if (strncasecmp(m_url, "https://", 8) == 0) {
+        m_url += 8;
+        m_url = strchr(m_url, '/');
+    }
+    if (!m_url || m_url[0] != '/') {
+        return BAD_REQUEST;
+    }
+    
+    std::cout << "The request url is: " << m_url << std::endl;
+    m_check_state = CHECK_STATE_HEADER;
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::parse_headers(char* text ) {
+    if (text[0] == '\0') {
+        // end of headers
+        if (m_content_length != 0) {
+            m_check_state = CHECK_STATE_CONTENT;
+            return NO_REQUEST;
+        }
+        return GET_REQUEST;
+    } else if (strncasecmp(text, "Connection:", 11) == 0) {
+        text += 11;
+        text += strspn(text, " \t");
+        if (strcasecmp(text, "keep-alive") == 0) {
+            m_linger = true;
+        }
+    } else if (strncasecmp(text, "Content-length:", 15) == 0) {
+        text += 15;
+        text += strspn(text, " \t");
+        m_content_length = atol(text);
+    } else if (strncasecmp(text, "Host:", 5) == 0) {
+        text += 5;
+        text += strspn(text, " \t");
+        m_host = text;
+    } else {
+        std::cout << "Oop! Unknown Header" << text << std::endl;
+    }
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::parse_content(char* text) {
+    if (m_read_idx >= m_checked_idx + m_content_length) {
+        text[m_content_length] = '\0';
+        std::cout << "request body: " << text << std::endl;
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::process_read() {
+    LINE_STATUS line_status = LINE_OK;
+    HTTP_CODE ret = NO_REQUEST;
+    char* text = 0;
+    while ((m_check_state == CHECK_STATE_CONTENT && line_status == LINE_OK) || 
+            (line_status = parse_line()) == LINE_OK) {
+        text = get_line();
+        m_start_line = m_checked_idx;
+        std::cout << "got 1 http line: " << text << std::endl;
+        switch (m_check_state) {
+            case CHECK_STATE_REQUESTLINE: {
+                ret = parse_request_line(text);
+                if (ret == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                }
+                break;
+            }
+            case CHECK_STATE_HEADER: {
+                ret = parse_headers(text);
+                if (ret == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                } else if (ret == GET_REQUEST) {
+                    return do_request();
+                }
+                break;
+            }
+            case CHECK_STATE_CONTENT: {
+                ret = parse_content(text);
+                if (ret == GET_REQUEST) {
+                    return do_request();
+                }
+                line_status = LINE_OPEN;
+                break;
+            }
+            default: {
+                return INTERNAL_ERROR;
+            }
+        }
+    }
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::do_request() {
+
+    return FILE_REQUEST;
+}
