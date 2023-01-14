e348b6758c83b97300329b3cbc67e25a3a12b028
Yinyu Wu <yinyu.wu@student.unsw.edu.au>
2023-01-14 17:20:47 +1100

    http_conn_test

--- webserver/include/threadpool.h
+++ webserver/include/threadpool.h
@@ -1,7 +1,11 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include <cstddef>
+#include <exception>
 #include <pthread.h>
+#include "./locker.h"
+#include <list>
 
 template<typename T>
 class threadpool {
@@ -10,13 +14,89 @@ class threadpool {
         ~threadpool();
         // add request to queue
         bool append(T* request);
+    
+    // HELPER FUNCTIONS
     private:
         static void* worker(void* arg);
         void run();
+    
+    // class members
     private:
         int m_thread_number;
         int m_max_request;
         pthread_t* m_threads;
+        std::list<T*> m_workqueue;
+        locker m_queuelocker;
+        sem m_queuestat;
+        bool m_stop;
 };
+
+template<typename T>
+threadpool<T>::threadpool(int thread_number, int max_request) :
+                m_thread_number(thread_number), m_max_request(max_request),
+                m_stop(false), m_threads(NULL){
+    if (thread_number <= 0 || max_request <= 0) {
+        throw std::exception();
+    }
+    m_threads = new pthread_t[m_thread_number];
+    if (!m_threads) {
+        throw std::exception();
+    }
+    
+    for (int i = 0; i < m_thread_number; ++i) {
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0) {
+            delete [] m_threads;
+            throw std::exception();
+        }
+        if (pthread_detach(m_threads[i])) {
+            delete [] m_threads;
+            throw std::exception();
+        }
+    }
+}
+
+template<typename T>
+threadpool<T>::~threadpool() {
+    delete [] m_threads;
+    m_stop = true;
+}
+
+template<typename T>
+bool threadpool<T>::append(T *request) {
+    m_queuelocker.lock();
+    if (m_workqueue.size() > m_max_request) {
+        m_queuelocker.unlock();
+        return false;
+    }
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+
+template<typename T>
+void* threadpool<T>::worker(void* arg) {
+    threadpool* pool = (threadpool*)arg;
+    pool->run();
+    return pool;
+}
+
+template<typename T>
+void threadpool<T>::run() {
+    while (!m_stop) {
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        if (m_workqueue.empty()) {
+            m_queuelocker.unlock();
+        }
+        T* request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_queuelocker.unlock();
+        if (!request) {
+            continue;
+        }
+        request->process();
+    }
+}
 
 #endif
